ce4d0fc59d5e16d03ddb57e2d2ea3204c6591113
Anton Danilov <a.deviun@gmail.com>
2018-07-27 09:55:08 +0300

    first look

---
+++ index.js
@@ -0,0 +1,19 @@
+// limit the numbers for one step
+const STEP_LIMIT = 256;
+
+/**
+ * @class ArrReplacer
+ * @description Replacer array of numbers to string
+ */
+class ArrReplacer {
+  /**
+   * @description Main replacer function
+   * @param {Array} arr Array of numbers
+   * @param {Number} stepLimit Limit the numbers for one step (by Default: STEP_LIMIT)
+   */
+  static async replace(arr, stepLimit = STEP_LIMIT) {
+    
+  }
+}
+
+module.exports = ArrReplacer;
